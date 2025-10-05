a5d355988327e2bad2daaa5a2c2b657049b7b94b
guoda23 <paulauskaite.guoda@gmail.com>
2025-10-05 10:26:43 +0200

    ran unconstrained reg experiments

--- test_visualizer_deterministic.py
+++ test_visualizer_deterministic.py
@@ -2,41 +2,38 @@
 # %%
 
 from OutputGenerator import OutputGenerator
-# batch_folder_deterministic = "Experiments/homophily_function_retrievability/deterministic/batch_2025-09-26_22-47-38"
+#constrained reg:
 batch_folder_deterministic = "Experiments/homophily_function_retrievability/deterministic/batch_2025-10-03_15-27-55"
+# unconstrained reg:
+# batch_folder_deterministic = "Experiments/homophily_function_retrievability/deterministic/batch_2025-10-04_16-30-32"
 
 
 # %%
 output_gen = OutputGenerator(batch_folder_deterministic, mode="deterministic")
 # %%
-output_gen._load_experiment_data()
-# output_gen.plot_real_entropy_boxplots(export=False)
+# output_gen._load_experiment_data()
+# # output_gen.plot_real_entropy_boxplots(export=False)
 
-#  %%
-df =output_gen.report_unreached_entropies()
-
-#%%
-
-# # %%
-# output_gen.plot_heatmaps(traits=['Gender_tertiary', 'Age_tertiary', 'EducationLevel_tertiary'], target_entropy=False,
-#                          dependent_variable="mean", vmax=0.45, save_path="Results/homophily_f_retrievability/heatmap_noise_0.30.png")
+# #  %%
+# df =output_gen.report_unreached_entropies()
 
 # %%
 output_gen.plot_heatmaps(traits=['Gender_tertiary', 'Age_tertiary', 'EducationLevel_tertiary'], target_entropy=True,
-                         dependent_variable="mean", vmax=0.45,
+                         dependent_variable="mean",
                          save_path="Results/homophily_f_retrievability/deterministic_heatmaps_mean.png",
+                         vmax=0.45,
                          suptitle=True)
 # %%
 output_gen.plot_heatmaps(traits=['Gender_tertiary', 'Age_tertiary', 'EducationLevel_tertiary'], target_entropy=True,
-                         dependent_variable="std", vmax=0.45,
+                         dependent_variable="std",
                          save_path="Results/homophily_f_retrievability/deterministic_heatmaps_std.png",
+                         vmax=0.45,
                          suptitle=True)
 
 # %%
 output_gen.plot_combined_heatmaps(img_mean="Results/homophily_f_retrievability/deterministic_heatmaps_mean.png",
         img_std="Results/homophily_f_retrievability/deterministic_heatmaps_std.png",
         combined_out="Results/homophily_f_retrievability/deterministic_heatmaps_combined.png",
-        title="Absolute Error of Regression Coefficients by Weight and Trait Entropy",
         figsize=(8, 8.5),
         show=True)
 
--- test_visualizer_stochastic.py
+++ test_visualizer_stochastic.py
@@ -2,7 +2,10 @@
 # %%
 
 from OutputGenerator import OutputGenerator
-batch_folder_stochastic = "Experiments/homophily_function_retrievability/stochastic/batch_2025-10-04_00-43-50"
+#constrained reg:
+# batch_folder_stochastic = "Experiments/homophily_function_retrievability/stochastic/batch_2025-10-04_00-43-50"
+#uncontrained reg:
+batch_folder_stochastic = "Experiments/homophily_function_retrievability/stochastic/batch_2025-10-04_22-52-24"
 
 
 # %%
@@ -14,16 +17,14 @@ output_gen = OutputGenerator(batch_folder_stochastic, mode="stochastic")
 # %%
 for noise_level in [0, 0.05, 0.10, 0.15, 0.25, 0.30]:
         output_gen._load_experiment_data(noise_level=noise_level)
-        # output_gen.plot_real_entropy_boxplots(export=False)
         custom_trait_labels = {
         'Gender_tertiary': f'σ = {noise_level:.2f}',}
-        # # %%
-        # df =output_gen.report_unreached_entropies()
 
         # %%
         output_gen.plot_heatmaps(traits=['Gender_tertiary'], target_entropy=True,
-                                dependent_variable="mean", vmax=0.45, 
-                                save_path=f"Results/homophily_f_retrievability/stochastic_heatmap_noise_{noise_level}.png",
+                                dependent_variable="mean", 
+                                vmax=0.45,
+                                save_path=f"Results/homophily_f_retrievability/unconstrained_stochastic_heatmap_noise_{noise_level}.png",
                                 suptitle=False, custom_trait_labels=custom_trait_labels, dpi=300)
 
 
@@ -43,6 +44,6 @@ for noise_level in [0, 0.05, 0.10, 0.15, 0.25, 0.30]:
 # output_gen.clean_noise_summary(batch_folder=batch_folder_stochastic)
 
 # #  %%
-# output_gen.plot_noise_vs_error(batch_folder=batch_folder_stochastic, save_path="Results/homophily_f_retrievability/stochastic_noise_vs_error.png")
+# output_gen.plot_noise_vs_error(batch_folder=batch_folder_stochastic, save_path="Results/homophily_f_retrievability/unconstrained_stochastic_noise_vs_error.png")
 # %%
 
